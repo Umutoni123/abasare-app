4a0f083ce41a3b8bb3d7fde234b517bbeb711e84
sebakunda <ericmaxkendrick@gmail.com>
2023-04-04 08:55:54 +0200

    styling the login and signup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
-import Navigation from './component/Navigation/Navigation';
-import Loginform from './component/Login/Loginform';
+import Navigation from './Navigation';
+import Signupform from './component/Login/Loginform';
 import Signup from './component/Signup/Signup';
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route} from 'react-router-dom';
 import Body from './component/body/Body';
+import Comform from './component/comform';
+import Digitals from './component/digitals';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      <Body/>
-      
-
-      <Routes>
+      {/* <Body/> */}
+    <Routes>
+        <Route path='/login' element={<Signupform />} />
         <Route path='/signup' element={<Signup />}/>
-        <Route path='/login' element={<Loginform />}/>
-
+        <Route path='/comform' element={<Comform />}/>
+        <Route path='/digitals' element={<Digitals />}/>
       </Routes>
     </div>
   );
--- src/component/Signup/Signup.jsx
+++ src/component/Signup/Signup.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+export default function Login() {
+const Navigate=useNavigate();
+    return (
+        <div className="relative flex flex-col justify-center min-h-screen  overflow-hidden"> 
+            <div className="w-full p-6 m-auto bg-[rgba(74, 148, 90, 0.1)] rounded-md shadow-xl text-green-500 lg:max-w-xl">
+                <h1 className="text-3xl font-semibold text-center text-green-500 underline uppercase 
+                ">
+                   Sign up
+                </h1>
+                <form className="mt-6">
+                    <div className="mb-2">
+                        <label
+                            for="email"
+                            className="block text-sm font-semibold   text-green-500"
+                        >
+                            
+                        </label>
+                        <input
+                            type="email" placeholder='Name'
+                            className="block w-full  rounded-xl px-4 py-2 mt-2  text-green-600  bg-gray-100 border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div> <br/> <br/>
+                    <div className="mb-2">
+                        <label
+                            for="password"
+                            className="block text-sm font-semibold  text-green-500"
+                        >
+                            
+                        </label>
+                        <input
+                            type="password" placeholder='Phone number'
+                            className="block w-full px-4 rounded-xl py-2 mt-2 text-green-500 bg-gray-100 border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div><br/> <br/>
+                    <div className="mb-2">
+                        <label
+                            for="password"  placeholder=''
+                            className="block text-sm font-semibold  text-green-500"
+                        >
+                        
+                        </label>
+                        <input
+                            type="password" placeholder='comform password'
+                            className="block w-full rounded-xl px-4 py-2 mt-2 text-green-500 bg-gray-100 border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div> <br/> 
+                    
+                    
+                    <div className="mt-6">
+                        <button  onClick={()=>Navigate('/comform')} className="w-[150px] rounded-xl px-4 py-2 tracking-wide bg-green-500    text-white transition-colors duration-200 transform bg-green-700 rounded-md hover: bg-white-600 focus:outline-none focus:bg-green-600">
+                            sign up
+                        </button>
+                    </div>
+                    
+                </form>
 
-export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+                <p className="mt-8 text-xs font-light text-center text-black">
+                    {" "}
+                     Have an account?{" "}
+                    <a
+                        href="#"
+                        className="font-medium text-green-500 hover:underline"
+                    >
+                        Sign up
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
 }
